63bd32caec6b6ed0e11f985d4f38494dd971c72c
wzt <1052987294@qq.com>
2022-03-16 04:50:33 +0800

    update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: "./dist"
@@ -27,15 +27,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                publicPath: '../',
-              },
-            },
-            'css-loader',
-          ],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
